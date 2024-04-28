cf394e0a4b92b46c6703cf1862989baeb92076c4
AlbericDeLajarte <albericlajarte@gmail.com>
2024-04-28 02:35:03 +0000

    added croccodyl based IK

--- src.py
+++ src.py
@@ -1,11 +1,13 @@
 import pinocchio
+import crocoddyl
 import copy
 import numpy as np
 from numpy.linalg import norm, solve
+import os
 
 class damped_IK():
 
-    def __init__(self, urdf_path, constraints, eps = 1e-3, max_iteration=1000, dt=0.1, damp=1e-12) -> None:
+    def __init__(self, urdf_path, constraints, eps = 1e-3, max_iteration=1000, dt=0.1, damp=1e-12, verbose=False) -> None:
         
         self.model = pinocchio.buildModelFromUrdf(urdf_path)
         self.data  = self.model.createData()
@@ -17,6 +19,8 @@ class damped_IK():
         self.max_iteration = max_iteration
         self.dt = dt
         self.damp = damp
+
+        self.verbose = verbose
 
         # Preprocess constraints
         for i in range(len(constraints)):
@@ -77,9 +81,157 @@ class damped_IK():
 
             # Fancy update of state q using pinocchio function
             q = pinocchio.integrate(self.model, q, joint_velocity*self.dt)
+            q[q<self.model.lowerPositionLimit] = self.model.lowerPositionLimit[q<self.model.lowerPositionLimit]
+            q[q>self.model.upperPositionLimit] = self.model.upperPositionLimit[q>self.model.upperPositionLimit]
             q_path.append(q)
 
             i += 1
 
+        if self.verbose:
+            pinocchio.forwardKinematics(self.model, self.data, q)
+            for constraint, target in zip(self.constraints, self.targets):
+                if constraint['type'] == 'position': 
+                    print("-----\nPosition constraint of joint ", constraint['frame'])
+                    print('Reached: ', self.data.oMi[constraint['frame']].translation)
+                    print('Target:', target.translation)
+
+                if constraint['type'] == 'orientation': 
+                    print("-----\nOrientation constraint of joint ", constraint['frame'])
+                    print('Reached:\n', self.data.oMi[constraint['frame']].rotation)
+                    print('Target:\n', target.rotation)
+
+
         return (q_path if return_path else q, 
                 success)
+    
+
+class croco_IK():
+
+    def __init__(self, urdf_path, constraints, eps = 1e-3, max_iteration=1000, dt=0.1, damp=1e-12, verbose=False) -> None:
+        
+        
+        # robot = pinocchio.robot_wrapper.RobotWrapper.BuildFromURDF(urdf_path, 
+        #                                                            os.path.join(*urdf_path.split('/')[:-1], '..', 'meshes'))
+
+        robot = pinocchio.robot_wrapper.RobotWrapper.BuildFromURDF('/home/albericlajarte/Desktop/example-robot-data/robots/panda_description/urdf/panda.urdf',
+                                                           '/home/albericlajarte/Desktop/example-robot-data/robots/panda_description/meshes')
+        
+        self.model = robot.model
+        # self.data  = self.model.createData()
+        self.state = crocoddyl.StateMultibody(self.model)
+
+        self.constraints = copy.deepcopy(constraints)
+
+        # Solver parameters
+        self.eps = eps
+        self.max_iteration = max_iteration
+        self.dt = dt
+        self.damp = damp
+
+        self.verbose = verbose
+
+        # # Preprocess constraints
+        # for i in range(len(constraints)):
+        #     self.constraints[i]['frame'] = self.model.getJointId(self.constraints[i]["frame"])
+        #     self.constraints[i]['weigth'] = np.ones(6)*self.constraints[i]['weigth']
+            
+        #     if self.constraints[i]['type'] == 'position':
+        #         self.constraints[i]['weigth'][-3:] = 0
+        #     elif self.constraints[i]['type'] == 'orientation':
+        #         self.constraints[i]['weigth'][:3] = 0
+        #     else:
+        #         print("Error in constraint type")
+
+    def run(self, q, targets, return_path=False):
+
+        state = crocoddyl.StateMultibody(self.model)
+
+        ## Create the cost functions ##
+        runningCostModel = crocoddyl.CostModelSum(state)
+        terminalCostModel = crocoddyl.CostModelSum(state)
+
+        # State constraint to reach target poses
+        for i, (constraint, target) in enumerate(zip(self.constraints, targets)):
+
+            if constraint['type'] == 'position':
+                new_cost = crocoddyl.CostModelResidual(
+                            state,
+                            crocoddyl.ResidualModelFrameTranslation(state, 
+                                                                    self.model.getFrameId(constraint['frame']), 
+                                                                    target))
+                
+                    
+            elif constraint['type'] == 'orientation':
+                new_cost = crocoddyl.CostModelResidual(
+                            state,
+                            crocoddyl.ResidualModelFrameRotation(state, 
+                                                                self.model.getFrameId(constraint['frame']), 
+                                                                target))
+                
+            runningCostModel.addCost(f"Pose{i}", new_cost, constraint['weigth'])
+            terminalCostModel.addCost(f"Pose{i}", new_cost, 100*constraint['weigth'])
+
+        # State and Control regularization and limits
+        activation_xbounds = crocoddyl.ActivationModelQuadraticBarrier(
+            crocoddyl.ActivationBounds( np.concatenate((self.model.lowerPositionLimit, -np.ones(state.nv)*100)), 
+                                        np.concatenate((self.model.upperPositionLimit, np.ones(state.nv)*100))
+                                        )
+        )
+    
+        xRegCost = crocoddyl.CostModelResidual(state, activation_xbounds, crocoddyl.ResidualModelState(state))
+        uRegCost = crocoddyl.CostModelResidual(state, crocoddyl.ResidualModelControl(state))
+
+        # Then let's added the running and terminal cost functions
+        runningCostModel.addCost("stateReg", xRegCost, 1e1)
+        runningCostModel.addCost("ctrlReg", uRegCost, 1e-7)
+        terminalCostModel.addCost("stateReg", xRegCost, 1e1)
+        terminalCostModel.addCost("ctrlReg", uRegCost, 1e-7)
+
+        ## Create the actuation model ##
+        actuationModel = crocoddyl.ActuationModelFull(state)
+
+        # Create the action model
+        runningModel = crocoddyl.IntegratedActionModelEuler(
+            crocoddyl.DifferentialActionModelFreeFwdDynamics(
+                state, actuationModel, runningCostModel
+            ),
+            self.dt,
+        )
+        terminalModel = crocoddyl.IntegratedActionModelEuler(
+            crocoddyl.DifferentialActionModelFreeFwdDynamics(
+                state, actuationModel, terminalCostModel
+            )
+        )
+
+        ## Solve problem ##
+        x0 = np.concatenate([q, np.zeros_like(q)])
+        problem = crocoddyl.ShootingProblem(x0, [runningModel] * self.max_iteration, terminalModel)
+
+        # Creating the DDP solver for this OC problem, defining a logger
+        ddp = crocoddyl.SolverDDP(problem)
+
+        # Solving it with the DDP algorithm
+        ddp.solve()
+
+        if self.verbose:
+            data = self.model.createData()
+            pinocchio.forwardKinematics(self.model, data, np.array(ddp.xs)[-1, :len(q)])
+            pinocchio.updateFramePlacements(self.model, data)
+
+            for constraint, target in zip(self.constraints, targets):
+                if constraint['type'] == 'position': 
+                    print("-----\nPosition constraint of joint ", constraint['frame'])
+                    print('Reached: ', data.oMf[self.model.getFrameId(constraint['frame'])].translation)
+                    print('Target:', target)
+
+                if constraint['type'] == 'orientation': 
+                    print("-----\nOrientation constraint of joint ", constraint['frame'])
+                    print('Reached:\n', data.oMf[self.model.getFrameId(constraint['frame'])].rotation)
+                    print('Target:\n', target)
+
+
+        return (np.array(ddp.xs)[:, :len(q)] if return_path else ddp.xs[-1],
+                ddp.isFeasible)
+
+
+        
